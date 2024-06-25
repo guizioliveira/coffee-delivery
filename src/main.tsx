42afeaf9e62b741579a32d2e769e5c26d92ec171
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-06-25 03:12:22 -0300

    feat: add base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import { App } from './app.tsx'
+import { defaultTheme } from './styles/themes/default.ts'
+import { GlobalStyle } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>,
 )
