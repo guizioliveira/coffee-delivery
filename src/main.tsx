dade1b168e6c657ff6fe023cd7f038344d220637
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-09-17 15:54:25 -0300

    feat: create routes system

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
 import { App } from './app.tsx'
-import { defaultTheme } from './styles/themes/default.ts'
-import { GlobalStyle } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
-      <App />
-      <GlobalStyle />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
 )
---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/defaultLayout'
+import { Home } from './pages/home'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/history" element={<History />} /> */}
+      </Route>
+    </Routes>
+  )
+}
