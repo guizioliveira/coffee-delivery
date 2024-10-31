80471033ffdd8a50cb2c8cd2b88fea6dae16c00e
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-10-31 02:18:16 -0300

    feat: create separator component

---
+++ src/components/ui/separator/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const StyledSeparator = styled.div`
+  width: 100%;
+  height: 1px;
+  padding: 0 1.5rem;
+  background-color: ${(props) => props.theme.colors.base.button};
+`
