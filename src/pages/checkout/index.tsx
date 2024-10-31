3d031baaa04f83ae9256f0eb28fd6c7f1f4af509
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-10-31 02:49:17 -0300

    feat: implement new components to checkout page

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,9 +1,12 @@
 import * as Box from '@/components/ui/box'
+import { CartItem } from './components/cart-item'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Spacing from '@/components/ui/spacing'
 import { Container, GridThreeColumns, GridTwoColumns, Section } from './styles'
 import { MapPinLine, CurrencyDollar } from 'phosphor-react'
+import { Separator } from '@/components/ui/separator'
+import Balance from './components/balance'
 
 export function Checkout() {
   return (
@@ -11,21 +14,26 @@ export function Checkout() {
       <Container>
         <Section size={640}>
           <h5>Complete seu pedido</h5>
+
           <Box.root
             title="Endereço de entrega"
             description="Informe o endereço onde deseja receber seu pedido"
             icon={{ color: 'yellow-dark', element: <MapPinLine /> }}
           >
             <Spacing apparence="xl" />
+
             <Box.content orientation="column">
               <GridThreeColumns>
                 <Input placeholder="CPF" />
               </GridThreeColumns>
+
               <Input placeholder="Endereço" />
+
               <GridTwoColumns>
                 <Input placeholder="Número" />
                 <Input placeholder="Complemento (Opcional)" />
               </GridTwoColumns>
+
               <GridThreeColumns>
                 <Input placeholder="Bairro" />
                 <Input placeholder="Cidade" />
@@ -33,6 +41,7 @@ export function Checkout() {
               </GridThreeColumns>
             </Box.content>
           </Box.root>
+
           <Box.root
             title="Pagamento"
             description="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
@@ -46,6 +55,16 @@ export function Checkout() {
         <Section size={448}>
           <h5>Cafés selecionados</h5>
           <Box.root>
+            <Box.content orientation="column">
+              <CartItem />
+              <Separator />
+              <CartItem />
+              <Separator />
+              <Balance total={29.7} shipping={3.5} />
+            </Box.content>
+
+            <Spacing apparence="l" />
+
             <Box.content>
               <Button>confirmar pedido</Button>
             </Box.content>
