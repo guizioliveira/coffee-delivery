1e015b840df610cdf5b58d746494f68467bfc1cb
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-12-02 23:01:03 -0300

    feat: consume api endpoint

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from 'react'
 import { Card } from './components/card'
 import { Hero } from './components/hero'
 import { ContentGrid } from './styles'
+import { Coffee } from '@/types'
 
 export function Home() {
+  const [coffees, setCoffees] = useState<Coffee[] | null>(null)
+
+  useEffect(() => {
+    fetch('/api/coffees')
+      .then((response) => response.json())
+      .then((data) => setCoffees(data.coffees))
+  }, [])
+
   return (
     <main>
       <Hero />
 
       <ContentGrid>
-        {Array.from({ length: 20 }).map((_, i) => (
-          <Card key={i} />
-        ))}
+        {coffees &&
+          coffees.map((coffee) => <Card key={coffee.id} data={coffee} />)}
       </ContentGrid>
     </main>
   )
