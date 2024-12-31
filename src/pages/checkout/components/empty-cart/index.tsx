6540fe68236e78c862b663522ce16ef4e10fa7c4
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2024-12-31 15:06:58 -0300

    feat: create `EmptyCart` component

---
+++ src/pages/checkout/components/empty-cart/index.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import { Content } from './styles'
+import { useNavigate } from 'react-router-dom'
+
+export default function EmptyCart() {
+  const navigate = useNavigate()
+
+  return (
+    <Content>
+      Você não tem itens no carrinho.
+      <Button onClick={() => navigate('/')}>
+        Escolha o melhor café para o seu momento
+      </Button>
+    </Content>
+  )
+}
---
+++ src/pages/checkout/components/empty-cart/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+  width: 100%;
+  padding: 4rem 0;
+
+  font: ${(props) => props.theme.fonts.baloo.titleS};
+
+  button {
+    width: auto;
+  }
+`
