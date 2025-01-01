b37e93f533561995fcfc9ee47ee66d92894d9f33
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2025-01-01 16:21:40 -0300

    chore: updating lint package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@guizioliveira/eslint-config/react'],
+
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_?key$',
+      },
+    ],
+  },
 }
